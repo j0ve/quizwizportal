f9a7c7cda19b8a7e8bfa6992037ca2a53593f1a2
Joris van Eil <joris@quizwiz.nl>
2017-02-08 17:03:09 +0100

    revert!

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname + '/../index.html'));
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
